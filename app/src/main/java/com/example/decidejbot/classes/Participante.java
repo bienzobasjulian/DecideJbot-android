48135f9d14a6308fe2c6b288d422ce2d9880c326
bienzobasjulian <bienzoj@gmail.com>
2022-05-16 12:33:31 +0200

    Create Participante class

---
+++ app/src/main/java/com/example/decidejbot/classes/Participante.java
@@ -0,0 +1,44 @@
+package com.example.decidejbot.classes;
+
+import com.orm.SugarRecord;
+
+public class Participante extends SugarRecord<Participante> {
+    
+    String valor;
+    Sorteo sorteo;
+
+
+    public Participante() {
+    }
+    
+
+    public Participante(String valor, Sorteo sorteo) {
+        this.valor = valor;
+        this.sorteo = sorteo;
+    }
+
+
+    public String getValor() {
+        return this.valor;
+    }
+
+    public void setValor(String valor) {
+        this.valor = valor;
+    }
+
+    public Sorteo getSorteo() {
+        return this.sorteo;
+    }
+
+    public void setSorteo(Sorteo sorteo) {
+        this.sorteo = sorteo;
+    }
+
+
+
+    
+    
+
+
+
+}
